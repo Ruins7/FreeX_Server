9e7e772ed3e1b24061595a230eb3b03d78efd08f
zibu15 <zhangzhao15@hotmail.com>
2016-11-08 14:00:07 -0500

    Updata For UserAction and BalanceAction
    UserAction: Add Logout( )
    BalanceAction: Add Deposite( ) and Withdrawal

---
+++ src/main/java/com/ece651/action/BalanceAction.java
@@ -0,0 +1,159 @@
+package com.ece651.action;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.Serializable;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+import net.sf.json.JSONObject;
+
+import org.apache.struts2.ServletActionContext;
+import org.apache.struts2.convention.annotation.Action;
+import org.apache.struts2.convention.annotation.Result;
+
+import com.ece651.entity.Balance;
+import com.ece651.entity.User;
+import com.ece651.service.BalanceService;
+import com.ece651.service.UserService;
+import com.opensymphony.xwork2.ActionSupport;
+
+/**
+ * @ClassName         BalanceAction.java
+ * @Description       deposit,withdrawal
+ * @author            Zhao
+ * @time              2016年11月8日 下午1:20:53
+ * @version			  v1.0
+ */
+public class BalanceAction extends ActionSupport {
+
+	private Balance balance;
+	private User user;
+	private HttpSession session;
+	private HttpServletRequest request = ServletActionContext.getRequest();
+	private HttpServletResponse response = ServletActionContext.getResponse();
+	private UserService userService;
+	private BalanceService balanceService;
+
+	// set注入
+	public void setUserService(UserService userService) {
+		this.userService = userService;
+	}
+
+	public void setBalanceService(BalanceService balanceService) {
+		this.balanceService = balanceService;
+	}
+
+	/**
+     * deposit
+     * @param  Balance(no need bid)
+     * @return succeed:Balance; failed:DepositFail
+     */
+	@Action(value = "deposit", results = { @Result(name = "success", type = "chain", location = "BalanceAddHistory") })
+	public String deposit() throws IOException {
+		// 设置JSON格式
+		request.setCharacterEncoding("utf-8");
+		response.setContentType("text/json;charset=utf-8");
+		// 通过bufferreader获取json数据
+		BufferedReader br = new BufferedReader(new InputStreamReader(
+				request.getInputStream(), "utf-8"));
+		StringBuffer sb = new StringBuffer("");
+		String temp = "";
+		while ((temp = br.readLine()) != null) {
+			sb.append(temp);
+		}
+		br.close();
+		// 将获取到的数据转换为JSONObjec
+		JSONObject reqObject = JSONObject.fromObject(sb.toString());
+		// 将JSONObject转换为对象
+		balance = new Balance();
+		balance = (Balance) JSONObject.toBean(reqObject, Balance.class);
+		// 调用userService，设置当前用户为balance中所指用户
+		user = new User();
+		user.setUid(balance.getBuid());
+		user = userService.searchUserByID(user);
+		// 调用balanceService层，获取当前用户的币种，如果没有则添加，如果有则直接更改余额
+		balance = balanceService.searchOneCurrOfUser(balance);
+		if (balance.getBuid() == 0) {
+			// this currency does not exist
+			Serializable bid = balanceService.addNewCurrencyBalance(balance);
+			if (bid != null) {
+				// add new currency succeed
+				balance.setBid(Integer.valueOf(bid.toString()));
+				JSONObject respObject = JSONObject.fromObject(balance);
+				this.response.setCharacterEncoding("UTF-8");
+				this.response.getWriter().write(respObject.toString());
+				return SUCCESS;
+			} else {
+				// adding new currency failed
+				this.response.getWriter().write("DepositFail");
+				return null;
+			}
+		} else {
+			// this currency exists and update currency
+			int ret = balanceService.deposit(balance);
+			if (ret == 1) {
+				// succeed
+				JSONObject respObject = JSONObject.fromObject(balance);
+				this.response.setCharacterEncoding("UTF-8");
+				this.response.getWriter().write(respObject.toString());
+				return SUCCESS;
+			} else {
+				// failed
+				this.response.getWriter().write("DepositFail");
+				return null;
+			}
+		}
+	}
+	
+	/**
+     * withdrawal
+     * @param  Balance(no need bid)
+     * @return succeed:Balance; failed:WithdrawalFail
+     */
+	@Action(value = "withdrawal", results = { @Result(name = "success", type = "chain", location = "BalancReduceHistory") })
+	public String withdrawal() throws IOException {
+		// 设置JSON格式
+		request.setCharacterEncoding("utf-8");
+		response.setContentType("text/json;charset=utf-8");
+		// 通过bufferreader获取json数据
+		BufferedReader br = new BufferedReader(new InputStreamReader(
+				request.getInputStream(), "utf-8"));
+		StringBuffer sb = new StringBuffer("");
+		String temp = "";
+		while ((temp = br.readLine()) != null) {
+			sb.append(temp);
+		}
+		br.close();
+		// 将获取到的数据转换为JSONObjec
+		JSONObject reqObject = JSONObject.fromObject(sb.toString());
+		// 将JSONObject转换为对象
+		balance = new Balance();
+		balance = (Balance) JSONObject.toBean(reqObject, Balance.class);
+		// 调用service层
+		// 调用balanceService层，获取当前用户的币种，如果没有则返回WithdrawalFailed，如果有则直接更改余额
+		balance.setBid(balanceService.searchOneCurrOfUser(balance).getBid());
+		if (balance.getBuid() == 0) {
+			// this currency does not exist
+			this.response.getWriter().write("WithdrawalFail");
+			return null;
+		} else {
+			// this currency exists and update currency
+			int ret = balanceService.withdrawal(balance);
+			if (ret == 1) {
+				// succeed
+				JSONObject respObject = JSONObject.fromObject(balance);
+				this.response.setCharacterEncoding("UTF-8");
+				this.response.getWriter().write(respObject.toString());
+				return SUCCESS;
+			} else {
+				// failed
+				this.response.getWriter().write("WithdrawalFail");
+				return null;
+			}
+		}
+	}
+}
--- src/main/java/com/ece651/action/UserAction.java
+++ src/main/java/com/ece651/action/UserAction.java
@@ -18,6 +18,14 @@ import com.ece651.entity.User;
 import com.ece651.service.UserService;
 import com.opensymphony.xwork2.ActionSupport;
 
+/**
+ * @ClassName         UserAction.java
+ * @Description       User: login, register
+ * @author            Zhao
+ * @time              2016年11月7日 下午8:20:53
+ * @version			  v1.0
+ */
+
 public class UserAction extends ActionSupport {
 
 	private User loginuser;
@@ -30,7 +38,12 @@ public class UserAction extends ActionSupport {
 	public void setUserService(UserService userService) {
 		this.userService = userService;
 	}
-
+	
+	/**
+     * login
+     * @param  User(no need uid)
+     * @return succeed:User(uid not 0); failed:LoginFail
+     */
 	@Action(value = "login")
 	public String login() throws IOException {
 		// 设置JSON格式
@@ -67,18 +80,61 @@ public class UserAction extends ActionSupport {
 			return null;
 		} else {
 			// 返回到登录界面,返回值为empty
-			response.getWriter().write("empty");
+			response.getWriter().write("LoginFail");
 			return null;
 		}
 	}
+	
+	/**
+     * logout
+     * @param  User(no need uid)
+     * @return succeed:User(uid not 0); failed:LogoutFail
+     */
+	@Action(value = "logout")
+	public String logout() throws IOException {
+		// 设置JSON格式
+		request.setCharacterEncoding("utf-8");
+		response.setContentType("text/json;charset=utf-8");
+		// 通过bufferreader获取json数据
+		BufferedReader br = new BufferedReader(new InputStreamReader(
+				request.getInputStream(), "utf-8"));
+		StringBuffer sb = new StringBuffer("");
+		String temp = "";
+		while ((temp = br.readLine()) != null) {
+			sb.append(temp);
+		}
+		br.close();
+		// 将获取到的数据转换为JSONObjec
+		JSONObject reqObject = JSONObject.fromObject(sb.toString());
+		// 将JSONObject转换为对象
+		loginuser = new User();
+		loginuser = (User) JSONObject.toBean(reqObject, User.class);
+		// 调用service层
+		loginuser = userService.login(loginuser);
 
-	@Action(value = "userinfo")
-	public String returnhistroy() throws IOException {
-		// 返回用户的历史记录
-		JSONObject respObject = new JSONObject();
-		return null;
+		// JSONObject respObject=new JSONObject();
+		// 结果判断
+		if (loginuser.getUid() != 0) {
+			// 首次登陆获取session来保存该用户的信息
+			session = request.getSession();
+			session.invalidate();
+			// user封装成JSON,返回给客户端
+			JSONObject respObject = JSONObject.fromObject(loginuser);
+			this.response.setCharacterEncoding("UTF-8");
+			this.response.getWriter().write(respObject.toString());
+			return null;
+		} else {
+			// 返回到登录界面,返回值为empty
+			response.getWriter().write("LogoutFail");
+			return null;
+		}
 	}
-
+	
+	/**
+     * register
+     * @param  User(no need uid)
+     * @return succeed:User(uid not 0); failed:RegisterFail
+     */
 	@Action(value = "register")
 	public String register() throws IOException {
 		// 设置JSON格式
@@ -99,7 +155,6 @@ public class UserAction extends ActionSupport {
 		loginuser = new User();
 		loginuser = (User) JSONObject.toBean(reqObject, User.class);
 		// 调用service层
-
 		Serializable uid = userService.signup(loginuser);
 		// 结果判断
 		if (uid != null) {
@@ -114,7 +169,7 @@ public class UserAction extends ActionSupport {
 			return null;
 		} else {
 			// 注册失败时返回;
-			response.getWriter().write("signupfail");
+			response.getWriter().write("RegisterFail");
 			return null;
 		}
 	}
